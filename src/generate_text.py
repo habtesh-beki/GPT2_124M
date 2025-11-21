9c3ade5825c4ab5c86d3888588c4c068d1a53a48
habtesh-beki <habteshbaki2014@gmail.com>
2025-11-21 07:24:17 -0500

    add generate text part

---
+++ src/generate_text.py
@@ -0,0 +1,74 @@
+import torch
+from src.GPT_Tokenizer import GPT_Tokenizer
+from src.gpt_model import GPTModel
+
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,    # Vocabulary size
+    "context_length": 1024, # Context length
+    "emb_dim": 768,         # Embedding dimension
+    "n_heads": 12,          # Number of attention heads
+    "n_layers": 12,         # Number of layers
+    "drop_rate": 0.1,       # Dropout rate
+    "qkv_bias": False       # Query-Key-Value bias
+}
+
+import tiktoken
+tokenizer = GPT_Tokenizer()
+batch = []
+txt1 = "Your hard work leads to"
+txt2 = "hard work is a key"
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+print(batch)
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+out = model(batch)
+print("Input batch:\n", batch)
+print("\nOutput shape:", out.shape)
+print(out)
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    # idx is (batch, n_tokens) array of indices in the current context
+    for _ in range(max_new_tokens):
+
+        # then only the last context_size tokens are used as context 10 token 5
+        idx_cond = idx[:, -context_size:]
+
+        # Get the predictions
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        # Focus only on the last time step
+        logits = logits[:, -1, :]
+        # Apply softmax to get probabilities
+        probas = torch.softmax(logits, dim=-1)  # (batch, vocab_size)
+
+        # Get the idx of the vocab entry with the highest probability value
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)  # (batch, 1)
+        # Append sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
+
+    return idx
+
+start_context = "Hello,I'm a developer"
+encoded = tokenizer.encode(start_context)
+print("encoded:", encoded)
+encoded_tensor = torch.tensor(encoded).unsqueeze(0) #A
+print("encoded_tensor.shape:", encoded_tensor.shape)
+
+model.eval() #A
+#model = GPTModel(GPT_CONFIG_124M)
+out = generate_text_simple(
+model=model,
+idx=encoded_tensor,
+max_new_tokens=10,
+context_size=GPT_CONFIG_124M["context_length"]
+)
+print("Output:", out)
+print("Output length:", len(out[0]))
+
+decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+print(decoded_text)
