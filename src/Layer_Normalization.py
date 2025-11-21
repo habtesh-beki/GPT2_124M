b445ae4f8f6d361587c3201cbe4c8aa595d6a466
habtesh-beki <habteshbaki2014@gmail.com>
2025-11-21 07:22:32 -0500

    add layer normalization

--- src/Layer_Normalization.py
+++ src/Layer_Normalization.py
@@ -1,17 +1,32 @@
+import torch
+torch.manual_seed(123)
+import torch.nn as nn
 """Now let's implement layer normalization"""
 
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,    # Vocabulary size
+    "context_length": 1024, # Context length
+    "emb_dim": 768,         # Embedding dimension
+    "n_heads": 12,          # Number of attention heads
+    "n_layers": 12,         # Number of layers
+    "drop_rate": 0.1,       # Dropout rate
+    "qkv_bias": False       # Query-Key-Value bias
+}
+
+"""Now let's implement layer normalization"""
+
+import torch.nn as nn
+
 class LayerNorm(nn.Module):
-    def __init__(self, d_in, eps=1e-5):
+    def __init__(self, emb_dim):
         super().__init__()
-        self.gamma = nn.Parameter(torch.ones(d_in))   # scale
-        self.beta  = nn.Parameter(torch.zeros(d_in))  # shift
-        self.eps = eps
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
 
     def forward(self, x):
-        # x shape: (batch, context_length, d_in)
-
-        mean = x.mean(dim=-1, keepdim=True)            # per token mean
-        var  = x.var(dim=-1, keepdim=True, unbiased=False)  # per token variance
-
-        x_norm = (x - mean) / torch.sqrt(var + self.eps)    # normalize
-        return self.gamma * x_norm + self.beta               # scale + shift
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
